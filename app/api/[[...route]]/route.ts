d7d28b511f93f561704358869a8715896bccf7ae
Eduardo <dudusoutomaior2006@gmail.com>
2024-08-28 02:10:50 -0300

    Routes update

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,25 +1,16 @@
+import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
-import { NextResponse } from 'next/server'
+import { z } from 'zod'
+import authors from './authors'
+import books from './books'
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app
-  .get('/hello', (c) => {
-        return c.json({
-            message: 'Hello Next.js!',
-        })
-    })
-  .get("/hello/:test", (c) => {
-        const test = c.req.param("test");
-
-        return c.json({
-            message: "Hello World!",
-            test: test,
-        })
-    })
+app.route("/authors", authors)
+app.route("/books", books)
 
 export const GET = handle(app)
 export const POST = handle(app)
